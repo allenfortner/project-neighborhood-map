b014577e1e0e304a04d7ec0563b4d593c42f9522
allenfortner <allenfortner99@gmail.com>
2018-11-27 17:12:15 -0500

    Add info windows for selected markers

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
-import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const API_Key = "AIzaSyCp9ZkXaTEwkTIOcrsu398dZtf1CGzeCbA";
 
 class MapContainer extends Component {
-	state = {}
+	state = {
+		activeMarker: {},
+		selectedPlace: {},
+		showingInfoWindow: false
+	}
+	
+	openInfoWindow = (props, marker, e) => {
+		console.log(props);
+		console.log(marker);
+		console.log(e);
+		//When a marker is clicked, open an info window for the selected place
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		})
+	}
+	
+	closeInfoWindow = () => {
+		//When the map itself is clicked, close current info window and clear the selected place
+		this.setState({
+			selectedPlace: {},
+			activeMarker: {},
+			showingInfoWindow: false
+		})
+	}
 	
 	render() {
 		const center = {lat: this.props.lat, lng: this.props.lng};
@@ -13,14 +38,29 @@ class MapContainer extends Component {
 			<Map 
 				google={this.props.google} 
 				initialCenter={center} 
-				zoom={this.props.zoom}>
+				zoom={this.props.zoom}
+				onClick={this.closeInfoWindow}>
 					{this.props.locations.map((loc, index) => {
-						return (<Marker
-							key={index}
-							name={loc.name}
-							position={loc.coordinates}
-						/>);
+						return (
+							<Marker
+								key={index}
+								name={loc.name}
+								address={loc.address}
+								city={loc.city}
+								state={loc.state}
+								zipcode={loc.zipcode}
+								position={loc.coordinates}
+								onClick={this.openInfoWindow}
+							/>
+						);
 					})}
+				<InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
+					<div className="infowindow-content">
+						<h2 className="infowindow-title">{this.state.selectedPlace.name}</h2>
+						<p className="infowindow-address">Address: {this.state.selectedPlace.address}, {this.state.selectedPlace.city}, {this.state.selectedPlace.state} {this.state.selectedPlace.zipcode}
+						</p>
+					</div>
+				</InfoWindow>
 			</Map>
 		);
 	}
